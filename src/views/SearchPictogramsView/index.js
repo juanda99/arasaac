bc1ed6bd223c46b2ec1c1adf9581879b4e033f4c
juanda99 <juandacorreo@gmail.com>
2016-02-23 19:00:54 +0100

    insert tabs in searchPictogramsView

--- src/views/SearchPictogramsView/index.js
+++ src/views/SearchPictogramsView/index.js
@@ -7,6 +7,10 @@ import Toggle from 'material-ui/lib/toggle'
 import IconButton from 'material-ui/lib/icon-button'
 // import ActionGrade from 'material-ui/lib/svg-icons/action/grade'
 import Filter from 'svg-icons/filter'
+import Tabs from 'material-ui/lib/tabs/tabs'
+import Tab from 'material-ui/lib/tabs/tab'
+// From https://github.com/oliviertassinari/react-swipeable-views
+import SwipeableViews from 'react-swipeable-views'
 
 const messages = defineMessages({
   search: {
@@ -28,6 +32,16 @@ const styles = {
   dropdown: {
     paddingLeft: 0,
     backgroundColor: 'red'
+  },
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400
+  },
+  slide: {
+    padding: 10,
+    overflow: 'hidden'
   }
 }
 
@@ -67,19 +81,37 @@ export class SearchPictogramsView extends Component {
             <Toggle label='Búsqueda avanzada' style={styles.toggle} />
           </div>
         </div>
-        <div className='row center-xs start-sm'>
-
+        <div className='row start-xs'>
           <SearchBox helpText={helpText} fullWidth={true} dataSource={fruit} />
           <RaisedButton label='Search' primary={true} style={styles.button} />
         </div>
         <div className='row'>
-          <IconButton tooltip='bottom-right' touch={true} tooltipPosition='bottom-right'>
-            <Filter/>
-          </IconButton>
-          <SelectCatalog/>
-          <SelectCatalog/>
-          <SelectCatalog/>
+          <div className='col-xs-12 col-sm-4'>
+            <IconButton tooltip='bottom-right' touch={true} tooltipPosition='bottom-right'>
+              <Filter/>
+            </IconButton>
+            <SelectCatalog/>
+          </div>
         </div>
+         <Tabs
+          onChange={this.handleChange}
+          value={0}
+        >
+          <Tab label='Resultados' value={0} />
+          <Tab label='Mi selección' value={1} />
+        </Tabs>
+        <SwipeableViews
+          index={0}
+          onChangeIndex={this.handleChange}
+        >
+          <div style={styles.slide}>
+            <h2 style={styles.headline}>Tabs with slide effect</h2>
+            Swipe to see the next slide.<br />
+          </div>
+          <div style={styles.slide}>
+            slide n°2
+          </div>
+        </SwipeableViews>
       </div>
     )
   }
