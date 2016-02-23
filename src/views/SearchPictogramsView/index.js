b26892300a7152ca4cda0eebc4489507a6063250
juanda99 <juandacorreo@gmail.com>
2016-02-23 10:29:28 +0100

    search pictograms header ok

--- src/views/SearchPictogramsView/index.js
+++ src/views/SearchPictogramsView/index.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react'
 import { defineMessages, FormattedMessage } from 'react-intl'
 import SearchBox from 'components/SearchBox.js'
-import DropdownCatalog from 'components/DropdownCatalog'
-
-import FloatingActionButton from 'material-ui/lib/floating-action-button'
-import ActionSearch from 'material-ui/lib/svg-icons/action/search'
-import ActionSettings from 'material-ui/lib/svg-icons/action/settings'
+import SelectCatalog from 'components/SelectCatalog'
+import RaisedButton from 'material-ui/lib/raised-button'
+import Toggle from 'material-ui/lib/toggle'
 
 const messages = defineMessages({
   search: {
@@ -22,59 +20,59 @@ const messages = defineMessages({
 
 const styles = {
   button: {
-    margin: 12,
+    margin: 22
+  },
+  dropdown: {
+    paddingLeft: 0,
+    backgroundColor: 'red'
   }
-};
+}
 
 export class SearchPictogramsView extends Component {
   render () {
-  const helpText = <FormattedMessage {...messages.search} />
-  const fruit = [
-    'Apple', 'Apricot', 'Avocado',
-    'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry',
-    'Boysenberry', 'Blood Orange',
-    'Cantaloupe', 'Currant', 'Cherry', 'Cherimoya', 'Cloudberry',
-    'Coconut', 'Cranberry', 'Clementine',
-    'Damson', 'Date', 'Dragonfruit', 'Durian',
-    'Elderberry',
-    'Feijoa', 'Fig',
-    'Goji berry', 'Gooseberry', 'Grape', 'Grapefruit', 'Guava',
-    'Honeydew', 'Huckleberry',
-    'Jabouticaba', 'Jackfruit', 'Jambul', 'Jujube', 'Juniper berry',
-    'Kiwi fruit', 'Kumquat',
-    'Lemon', 'Lime', 'Loquat', 'Lychee',
-    'Nectarine',
-    'Mango', 'Marion berry', 'Melon', 'Miracle fruit', 'Mulberry', 'Mandarine',
-    'Olive', 'Orange',
-    'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Physalis', 'Plum', 'Pineapple',
-    'Pumpkin', 'Pomegranate', 'Pomelo', 'Purple Mangosteen',
-    'Quince',
-    'Raspberry', 'Raisin', 'Rambutan', 'Redcurrant',
-    'Salal berry', 'Satsuma', 'Star fruit', 'Strawberry', 'Squash', 'Salmonberry',
-    'Tamarillo', 'Tamarind', 'Tomato', 'Tangerine',
-    'Ugli fruit',
-    'Watermelon'
-  ]
+    const helpText = <FormattedMessage {...messages.search} />
+    const fruit = [
+      'Apple', 'Apricot', 'Avocado',
+      'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry',
+      'Boysenberry', 'Blood Orange',
+      'Cantaloupe', 'Currant', 'Cherry', 'Cherimoya', 'Cloudberry',
+      'Coconut', 'Cranberry', 'Clementine',
+      'Damson', 'Date', 'Dragonfruit', 'Durian',
+      'Elderberry',
+      'Feijoa', 'Fig',
+      'Goji berry', 'Gooseberry', 'Grape', 'Grapefruit', 'Guava',
+      'Honeydew', 'Huckleberry',
+      'Jabouticaba', 'Jackfruit', 'Jambul', 'Jujube', 'Juniper berry',
+      'Kiwi fruit', 'Kumquat',
+      'Lemon', 'Lime', 'Loquat', 'Lychee',
+      'Nectarine',
+      'Mango', 'Marion berry', 'Melon', 'Miracle fruit', 'Mulberry', 'Mandarine',
+      'Olive', 'Orange',
+      'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Physalis', 'Plum', 'Pineapple',
+      'Pumpkin', 'Pomegranate', 'Pomelo', 'Purple Mangosteen',
+      'Quince',
+      'Raspberry', 'Raisin', 'Rambutan', 'Redcurrant',
+      'Salal berry', 'Satsuma', 'Star fruit', 'Strawberry', 'Squash', 'Salmonberry',
+      'Tamarillo', 'Tamarind', 'Tomato', 'Tangerine',
+      'Ugli fruit',
+      'Watermelon'
+    ]
     return (
-      <div className='row center-xs start-sm'>
-      <div class='col-sm-offset-3'>
-        <SearchBox helpText={helpText} fullWidth={true} dataSource={fruit} />
-        <FloatingActionButton style={styles.button}>
-          <ActionSearch />
-        </FloatingActionButton>
-        <div>
-          <DropdownCatalog underlineStyle={{margin: '5px'}} />
-
-          <FloatingActionButton secondary={true} style={styles.button}>
-            <ActionSettings />
-          </FloatingActionButton>
+      <div>
+        <div className='row end-xs'>
+          <div className='col-xs-6 col-sm-4 col-md-3'>
+            <Toggle label='Búsqueda avanzada' style={styles.toggle} />
+          </div>
         </div>
-        <div>
-          <DropdownCatalog/>
-          <DropdownCatalog/>
-          <DropdownCatalog/>
+        <div className='row start-sm'>
+          <SearchBox helpText={helpText} fullWidth={true} dataSource={fruit} />
+          <RaisedButton label='Search' primary={true} style={styles.button} />
         </div>
-      </div>
+        <div className='row'>
+            <SelectCatalog/>
+            <SelectCatalog/>
+            <SelectCatalog/>
+        </div>
       </div>
     )
   }
