36690b87f456ccd9f89b8e7452ebfa6db44c130f
juanda99 <juandacorreo@gmail.com>
2016-02-29 12:24:35 +0100

    chore(CI): run linter before any other checks

--- src/svg-icons/filter.jsx
+++ src/svg-icons/filter.jsx
@@ -4,7 +4,7 @@ import SvgIcon from 'material-ui/lib/svg-icon'
 
 let Filter = (props) => (
   <SvgIcon {...props}>
-	<path d='M3,2H21V2H21V4H20.92L14,10.92V22.91L10,18.91V10.91L3.09,4H3V2Z'/>
+    <path d='M3,2H21V2H21V4H20.92L14,10.92V22.91L10,18.91V10.91L3.09,4H3V2Z'/>
   </SvgIcon>
 )
 Filter = pure(Filter)
