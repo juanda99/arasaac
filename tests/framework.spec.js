def930075acbbe43f7ec5cac4fb15770fa1b26e2
juanda99 <juandacorreo@gmail.com>
2016-02-29 14:59:07 +0100

    fix(chai-enzyme): fix issues when using cheerio when chai, add tests to confirm

--- tests/framework.spec.js
+++ tests/framework.spec.js
@@ -1,4 +1,17 @@
 import assert from 'assert'
+import React, {Component} from 'react'
+import {mount, render, shallow} from 'enzyme'
+
+class Fixture extends Component {
+  render () {
+    return (
+      <div>
+        <input id='checked' defaultChecked />
+        <input id='not' defaultChecked={false} />
+      </div>
+    )
+  }
+}
 
 describe('(Framework) Karma Plugins', function () {
   it('Should expose "expect" globally.', function () {
@@ -10,12 +23,21 @@ describe('(Framework) Karma Plugins', function () {
   })
 
   it('Should have chai-as-promised helpers.', function () {
-    const pass = new Promise(res => res('test'))
-    const fail = new Promise((res, rej) => rej())
+    const pass = new Promise((resolve) => resolve('test'))
+    const fail = new Promise((resolve, reject) => reject())
 
     return Promise.all([
       expect(pass).to.be.fulfilled,
       expect(fail).to.not.be.fulfilled
     ])
+  })
+
+  it('should have chai-enzyme working', function () {
+    let wrapper = shallow(<Fixture />)
+    expect(wrapper.find('#checked')).to.be.checked()
+    wrapper = mount(<Fixture />)
+    expect(wrapper.find('#checked')).to.be.checked()
+    wrapper = render(<Fixture />)
+    expect(wrapper.find('#checked')).to.be.checked()
   })
 })
