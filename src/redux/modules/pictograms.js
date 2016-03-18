3e15aa34351abfd486a6515ce44f8bdd96cc72ed
juanda <juandacorreo@gmail.com>
2016-03-18 11:15:34 +0100

    actions for pictograms with pagination

--- src/redux/modules/pictograms.js
+++ src/redux/modules/pictograms.js
@@ -6,22 +6,28 @@ export const PICTOGRAMS_FAILURE = 'PICTOGRAMS_FAILURE'
 
 // Fetches all keywords for pictos
 // Relies on the custom API middleware defined in ../middleware/api.js.
-function fetchPictograms(searchText) {
+function fetchPictograms(searchText, nextPageUrl) {
   return {
+    searchText,
     [CALL_API]: {
       types: [ PICTOGRAMS_REQUEST, PICTOGRAMS_SUCCESS, PICTOGRAMS_FAILURE ],
-      endpoint: `search/${searchText}`,
+      endpoint: nextPageUrl,
       schema: Schemas.SEARCH
     }
   }
 }
 
-export function loadPictograms(searchText) {
+export function loadPictograms(searchText, nextPage) {
   return (dispatch, getState) => {
-    const search = getState().entities.searchs[searchText]
-    if (search) {
+    const {
+      nextPageUrl = `search/${searchText}`,
+      pageCount = 0
+    } = getState().pagination.pictogramsBySearchText[searchText] || {}
+
+    if (pageCount > 0 && !nextPage) {
       return null
     }
-    return dispatch(fetchPictograms(searchText))
+    return dispatch(fetchPictograms(searchText, nextPageUrl))
   }
 }
+
