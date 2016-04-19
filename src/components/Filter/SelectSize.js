6d6d8f5d0e4d7421f5af0fe558e835506f3e7df9
juanda <juandacorreo@gmail.com>
2016-04-19 10:59:42 +0200

    lint fixes

--- src/components/Filter/SelectSize.js
+++ src/components/Filter/SelectSize.js
@@ -73,9 +73,9 @@ class SelectCatalog extends Component {
         {this.state.value !== 1 ? <FilterIcon /> : null}
         <SelectField autoWidth={true} value={this.state.value} onChange={this.handleChange} style={styles.select}>
           <MenuItem value={1} label={<FormattedMessage {...messages.size} />} primaryText={<FormattedMessage {...messages.any} />} />
-          <MenuItem value={2} label={<FormattedMessage {...messages.large} />}  primaryText={<FormattedMessage {...messages.largeChoose} />} />
-          <MenuItem value={3} label={<FormattedMessage {...messages.medium} />}  primaryText={<FormattedMessage {...messages.mediumChoose} />} />
-          <MenuItem value={4} label={<FormattedMessage {...messages.small} />}  primaryText={<FormattedMessage {...messages.smallChoose} />} />
+          <MenuItem value={2} label={<FormattedMessage {...messages.large} />} primaryText={<FormattedMessage {...messages.largeChoose} />} />
+          <MenuItem value={3} label={<FormattedMessage {...messages.medium} />} primaryText={<FormattedMessage {...messages.mediumChoose} />} />
+          <MenuItem value={4} label={<FormattedMessage {...messages.small} />} primaryText={<FormattedMessage {...messages.smallChoose} />} />
         </SelectField>
       </span>
     )
