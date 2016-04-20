7867e698bd99d53f780f79e9a27a306e771606b9
juanda <juandacorreo@gmail.com>
2016-04-20 09:42:26 +0200

    change action type name

--- src/redux/modules/showFilter.js
+++ src/redux/modules/showFilter.js
@@ -1,15 +1,15 @@
-export const TOGGLE_FILTER = 'TOGGLE_FILTER'
+export const SHOW_FILTER = 'SHOW_FILTER'
 
 export function toggleShowFilter(text) {
   return {
-    type: TOGGLE_FILTER
+    type: SHOW_FILTER
   }
 }
 
 // default state should also be default state for toggle component
 export default function showFilter(state = false, action) {
   switch (action.type) {
-    case TOGGLE_FILTER:
+    case SHOW_FILTER:
       return !state
     default:
       return state
