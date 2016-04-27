dac511fc3e7885bafc368f0accf938cc64298894
juanda <juandacorreo@gmail.com>
2016-04-27 12:00:01 +0200

    lint error

--- src/svg-icons/filter.jsx
+++ src/svg-icons/filter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import pure from 'recompose/pure'
 import SvgIcon from 'material-ui/lib/svg-icon'
 
-let Filter = (props) => (
+let Filter = props => (
   <SvgIcon {...props}>
     <path d='M3,2H21V2H21V4H20.92L14,10.92V22.91L10,18.91V10.91L3.09,4H3V2Z'/>
   </SvgIcon>
