a184832d6734a34e8b0021b03be09d719986b60c
juanda <juandacorreo@gmail.com>
2016-04-28 15:37:23 +0200

    show register options previous to signup

---
+++ src/components/RegisterOptions.js
@@ -0,0 +1,162 @@
+import React, {Component, PropTypes} from 'react'
+import RaisedButton from 'material-ui/lib/raised-button'
+import Paper from 'material-ui/lib/paper'
+import Colors from 'material-ui/lib/styles/colors'
+import ArasaacLogo from 'images/arasaac-logo.svg'
+import GoogleIcon from './icons/GoogleIcon'
+import FacebookIcon from './icons/FacebookIcon'
+import EmailIcon from 'material-ui/lib/svg-icons/communication/email'
+import { defineMessages, FormattedMessage } from 'react-intl'
+import { Link } from 'react-router'
+
+const messages = defineMessages({
+  google: {
+    id: 'signup.google',
+    description: 'Sign up Google button',
+    defaultMessage: 'Continue with Google'
+  },
+  facebook: {
+    id: 'signup.facebook',
+    description: 'Signin Facebook button',
+    defaultMessage: 'Continue with Facebook'
+  },
+  or: {
+    id: 'signup.or',
+    description: 'Or, because it offers two posibilities',
+    defaultMessage: 'or'
+  },
+  user: {
+    id: 'user.signin',
+    description: 'Username field default text for login',
+    defaultMessage: 'User'
+  },
+  offerSignin: {
+    id: 'signin.offerAccount',
+    description: 'Text inviting for creating an account',
+    defaultMessage: 'Already an Arasaac user?'
+  },
+  signup: {
+    id: 'signin.signup',
+    description: 'Button for creating a new account',
+    defaultMessage: 'Sign up with email'
+  },
+  signin: {
+    id: 'signup.signin',
+    description: 'Button for going to sign in view',
+    defaultMessage: 'Sign in'
+  }
+})
+
+const styles = {
+  separator: {
+    textAlign: 'center',
+    clear: 'both'
+  },
+  separatorText: {
+    display: 'inlineBlock',
+    padding: 8,
+    position: 'relative',
+    backgroundColor: '#fff'
+  },
+  paper: {
+    padding: 20,
+    width: 400,
+    margin: '0 auto',
+    marginTop: 100,
+    marginBottom: 130
+  },
+  separatorLine: {
+    margin: '-10px auto 10px'
+  },
+  googleButton: {
+    width: '100%',
+    float: 'left',
+    marginBottom: 5
+  },
+  facebookButton: {
+    float: 'right',
+    width: '100%',
+    marginBottom: 30,
+    top: -10
+  },
+  text: {
+    width: '100%'
+  },
+  register: {
+    position: 'absolute',
+    right: 8,
+    marginTop: 5
+  },
+  signupButton: {
+    width: '100%'
+  },
+  svgLogo: {
+    width: 180,
+    marginLeft: 70
+  },
+  signin: {
+    position: 'absolute',
+    right: 8,
+    marginTop: 5
+  }
+}
+
+class RegisterOptions extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+    showRegister: PropTypes.func.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.props.showRegister(true)
+  }
+
+  render() {
+    return (
+      <Paper zDepth={2} style={styles.paper}>
+        <div className='row'>
+          <div className='col-xs-12 col-sm-6'>
+            <img style={styles.svgLogo} src={ArasaacLogo} />
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <RaisedButton style={styles.googleButton} backgroundColor={Colors.red500}
+              label={<FormattedMessage {...messages.google} />} primary={true} icon={<GoogleIcon />}/>
+            <RaisedButton style={styles.facebookButton} backgroundColor={Colors.blue500}
+              label={<FormattedMessage {...messages.facebook} />} primary={true} icon={<FacebookIcon/>}/>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <div style={styles.separator}>
+              <span style={styles.separatorText}>{<FormattedMessage {...messages.or} />}</span>
+              <hr style={styles.separatorLine} />
+            </div>
+          </div>
+        </div>
+        <div className='row' style={{marginTop: 10}}>
+          <div className='col-xs-12'>
+            <RaisedButton style={styles.googleButton} onClick={this.handleClick}
+              label={<FormattedMessage {...messages.signup} />} primary={true} icon={<EmailIcon />}/>
+          </div>
+        </div>
+        <div className='row' style={{marginTop: 10}}>
+          <div className='col-xs-8' style={{textAlign: 'right'}}>
+            <p style={{textAlign: 'left'}}>{<FormattedMessage {...messages.offerSignin} />}</p>
+          </div>
+          <div className='col-xs-4' style={{position: 'relative'}}>
+            <Link to='/signin'><RaisedButton style={styles.signin} label={<FormattedMessage {...messages.signin} />} secondary={true} /></Link>
+          </div>
+        </div>
+      </Paper>
+    )
+  }
+}
+export default RegisterOptions
+
--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react'
 import { connect } from 'react-redux'
 import RegisterForm from 'components/RegisterForm'
-
+import RegisterOptions from 'components/RegisterOptions'
 export default class RegisterView extends Component {
   static propTypes = {
     name: PropTypes.string
@@ -9,11 +9,23 @@ export default class RegisterView extends Component {
 
   constructor(props) {
     super(props)
+    this.showRegister = this.showRegister.bind(this)
+    /* show pre register, so user can choose signup option */
+    this.state = {
+      showRegister: false
+    }
+  }
+
+  showRegister(value) {
+    this.setState({showRegister: value})
   }
 
   render() {
+    let showRegister = this.state.showRegister
     return (
-      <RegisterForm/>
+      <div>
+        { showRegister ?  <RegisterForm/> : <RegisterOptions showRegister={this.showRegister}/> }
+      </div>
     )
   }
 }
