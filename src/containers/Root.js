e9be90cc37b0f1bd8d1f0cfaa19be6a202817fa8
juanda <juandacorreo@gmail.com>
2016-09-19 11:15:19 +0200

    fix imports

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,9 +1,8 @@
 import React, { PropTypes } from 'react'
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import { Router } from 'react-router'
 import { IntlProvider } from 'react-intl'
 import * as messages from 'i18n/'
-import { connect } from 'react-redux'
 
 class Root extends React.Component {
   static propTypes = {
